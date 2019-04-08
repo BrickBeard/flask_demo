c33543f318f5f3635e24d64ce395f54509890a4b
Brandon Smith <brandon.smith@tbsokc.com>
2019-04-08 10:49:39 -0500

    Added 'users_count' to companies endpoint

--- project/api/views.py
+++ project/api/views.py
@@ -36,6 +36,8 @@ def get_user(id):
 @api_bp.route('/companies')
 def get_companies():
     company_list = Company.query.order_by(Company.id).all()
-    company_list = [company.serialize for company in company_list]    
-    print(f'company_list: {company_list}')
+    company_list = [company.serialize for company in company_list]
+    for company in company_list:
+        company['users_count'] = len(User.query.filter_by(company_id=company['id']).all())
+        company['company_revenue'] = float(company['company_revenue'])
     return  jsonify(company_list), 200
