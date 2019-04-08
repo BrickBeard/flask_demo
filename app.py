fd6659e9cae4407bc9c8011a227855680f9c1dc1
Brandon Smith <brandon.smith@tbsokc.com>
2019-04-08 14:57:14 -0500

    Revised database management commands

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask_migrate import Migrate
 from project import app
+from project.models import User, Company, db
+
+
+# Database Management  ('flask db migrate' & 'flask db upgrade')
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Company=Company)
