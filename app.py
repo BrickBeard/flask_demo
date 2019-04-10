eeee6b634f137fdd578025d0ca2c5ad9f404c866
Brandon Smith <brandon.smith@tbsokc.com>
2019-04-10 10:23:37 -0500

    Updated migration code required

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask_migrate import Migrate
 from project import app
-from project.models import User, Company, db
+from project.models import db
 
 
 # Database Management  ('flask db migrate' & 'flask db upgrade')
 migrate = Migrate(app, db)
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(app=app, db=db, User=User, Company=Company)
